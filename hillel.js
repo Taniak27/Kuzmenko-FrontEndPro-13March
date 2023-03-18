5187a698dde70fef3b11e7465c89d343845b4a4c
Taniak27 <127733847+Taniak27@users.noreply.github.com>
2023-03-18 11:51:45 +0200

    Adding of HW_3

---
+++ hillel.js
@@ -0,0 +1,29 @@
+/*
+let role = 'The student - ';
+let surname = 'Tatiana Kuzmenko ';
+let location = 'is from Odesa.';
+
+console.log(role + surname + location);
+
+let firstNumber = '5';
+let secondNumber = '9';
+let thirdNumber = '1';
+let fourthNumber = '2';
+let fifthNumber = '0';
+
+console.log(firstNumber + ' ' + secondNumber + ' ' + thirdNumber + ' ' + fourthNumber + ' ' + fifthNumber);
+*/
+
+//HOME_WORK_3
+
+const firstNumber = parseInt(prompt('Enter the first number'));
+const secondNumber = parseInt(prompt('Enter the second number'));
+let adding = firstNumber + secondNumber;
+let subctraction = firstNumber - secondNumber;
+let multiplying = firstNumber * secondNumber;
+let division = firstNumber / secondNumber;
+
+alert(adding);
+alert(subctraction);
+alert(multiplying);
+alert(division);
