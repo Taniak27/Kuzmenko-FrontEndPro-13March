19043c18a0838ae151b8b3a7377da5307cdbcc9f
Taniak27 <127733847+Taniak27@users.noreply.github.com>
2023-04-08 18:19:50 +0300

    Create HomeWork#13.js

---
+++ HomeWork#13.js
@@ -0,0 +1,18 @@
+//HOMEWORK_13
+// Реалізуйте функцію generateKey(length, characters), яка повертає рядок випадкових символів із набору characters довжиною length
+
+
+const characters = 'abcdefghijklmnopqrstuvwxyz0123456789';
+
+function generateKey(length) {
+  let result = ' ';
+  const charactersLength = characters.length;
+  for ( let i = 0; i < length; i++ ) {
+      result += characters.charAt(Math.floor(Math.random() * charactersLength));
+  }
+
+  return result;
+}
+
+const key = generateKey(16, characters);
+console.log(key);
