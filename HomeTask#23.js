354cd2650849edb72fe740e4b5796cf655ea6199
Taniak27 <127733847+Taniak27@users.noreply.github.com>
2023-05-12 18:38:40 +0300

    Create HomeTask#23.js

---
+++ HomeTask#23.js
@@ -0,0 +1,10 @@
+//HOMETASK_23
+
+/*Треба реалізувати просту програму з наступними умовами:
+За допомогою prompt запитати ім'я користувача.
+За допомогою alert вивести "Hello, John! How are you?" , де John це те, що ввів користувач
+*/
+
+let name = prompt('What is your name?');
+
+alert(`Hello, ${name}! How are you?`);
