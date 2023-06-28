81432e1497253276d65a2920b15917b6b90b7a73
Taniak27 <127733847+Taniak27@users.noreply.github.com>
2023-06-28 17:33:48 +0300

    Create HomeTask#39.js

---
+++ HomeTask#39.js
@@ -0,0 +1,19 @@
+// Виведення загальної ширини та висоти екрану
+console.log("Screen Width: " + screen.width + "px");
+console.log("Screen Height: " + screen.height + "px");
+
+// Виведення доступної ширини та висоти екрану
+console.log("Available Screen Width: " + screen.availWidth + "px");
+console.log("Available Screen Height: " + screen.availHeight + "px");
+
+/*
+https://raw.githubusercontent.com/Taniak27/Pictures/main/Снимок%20экрана%202023-06-28%20в%2017.31.23.png
+*/
+
+/*
+Приклад показує використання властивостей screen.width і screen.height для виведення загальних розмірів екрану в пікселях. 
+Ці значення вказують на загальну ширину та висоту екрану користувача.
+
+Інший приклад показує використання властивостей screen.availWidth і screen.availHeight для виведення доступних розмірів екрану в пікселях. 
+Ці значення вказують на ширину та висоту екрану, які доступні для відображення віконного контенту, не враховуючи панелі інструментів, смуг прокрутки або інші елементи інтерфейсу браузера.
+*/
